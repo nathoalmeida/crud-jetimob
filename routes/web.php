aea861291b951818bfd2a7d6f41d47872c179a77
nathoalmeida <nathalia.oalmeida@gmail.com>
2023-10-24 09:19:33 -0300

    correção routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/livros/novo', 'LivrosController@create');
+Route::get('/livros/novo' , 'App\Http\Controllers\LivrosController@create');
